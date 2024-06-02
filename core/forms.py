fd075fc4f8f55f634d3ce2c9d0eefabde91aac55
mubaarock <mustophamubarack17@gmail.com>
2024-06-02 14:08:49 +0100

    Add model forms for Rating and Restaurant

---
+++ core/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Rating, Restaurant
+
+
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = "__all__"
+
+
+class RestaurantForm(forms.ModelForm):
+    class Meta:
+        model = Restaurant
+        fields = ("name", "restuarant_type")
