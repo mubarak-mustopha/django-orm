d786f9870fd70d538ac2dea43a75fac9f97ddafb
mubaarock <mustophamubarack17@gmail.com>
2024-06-02 14:05:33 +0100

    Add view to handle request for index page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import RatingForm, RestaurantForm
 
 # Create your views here.
+
+
+def index(request):
+    if request.method == "POST":
+        form = RestaurantForm(request.POST or None)
+        if form.is_valid():
+            print(form.cleaned_data)
+        else:
+            print(form.errors)
+            return render(request, "index.html", {"form": form})
+
+    context = {"form": RestaurantForm()}
+    return render(request, "index.html", context)
