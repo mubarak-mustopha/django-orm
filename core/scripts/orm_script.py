86d8b563de7dcfab3c4bab5aff0e7953ddb770d0
mubaarock <mustophamubarack17@gmail.com>
2024-06-17 15:24:05 +0100

    Aggregation and annotations with django or[Dm

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -2,16 +2,26 @@ from django.contrib.auth.models import User
 from core.models import Restaurant, Rating, Sale, Staff, StaffRestaurant
 from django.utils import timezone
 from django.db import connection
-from django.db.models.functions import Lower, Upper
+from django.db.models.functions import Lower, Upper, Length, Concat
+from django.db.models import CharField, Value, Avg, Sum, Count
 from pprint import pprint as pp
 import random
 
 
 def run():
-    restaurants = Restaurant.objects.values_list("name", flat=True)
+    # group by all cols in restaurant table
+    rs = Restaurant.objects.annotate(
+        num_ratings=Count("ratings"),
+        avg_rating=Avg("ratings__rating"),
+    )
+    print(rs.values("name", "num_ratings", "avg_rating"))
 
-    print(restaurants)
-    # pp(connection.queries)
+    # group by single column
+    rs = Restaurant.objects.values("restaurant_type").annotate(
+        avg_rating=Avg("ratings__rating"),
+    )
+    print(rs)
+    pp(connection.queries)
 
 
 # shell_plus --print-sql
