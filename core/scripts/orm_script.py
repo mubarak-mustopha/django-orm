51eb9f3d16c3f19dc40789bebf378aa90eaed83e
mubaarock <mustophamubarack17@gmail.com>
2024-06-21 11:37:35 +0100

    Add filter statements to aggregations

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -3,18 +3,18 @@ from core.models import Restaurant, Rating, Sale, Staff, StaffRestaurant
 from django.utils import timezone
 from django.db import connection
 from django.db.models.functions import Lower, Upper, Length, Concat
-from django.db.models import F
+from django.db.models import F, Count, Q
 from pprint import pprint as pp
 import random
 
 
 def run():
-    sales = Sale.objects.annotate(profit=F("income") - F("expenditure")).values(
-        "income",
-        "expenditure",
-        "profit",
+    sales = Sale.objects.aggregate(
+        profit=Count("id", filter=Q(income__gt=F("expenditure"))),
+        loss=Count("id", filter=Q(income__lt=F("expenditure"))),
     )
-    print(sales[0])
+    print(sales)
+
     pp(connection.queries)
 
 
