4871376dc38ca05422f99fce9086ce398e96e831
mubaarock <mustophamubarack17@gmail.com>
2024-06-26 03:30:01 +0100

    COALESCE func and default kwarg act similarly in d django orm

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -2,17 +2,21 @@ from django.contrib.auth.models import User
 from core.models import Restaurant, Rating, Sale, Staff, StaffRestaurant
 from django.utils import timezone
 from django.db import connection
-from django.db.models.functions import Lower, Upper, Length, Concat
-from django.db.models import F, Count, Q
+from django.db.models.functions import Lower, Upper, Length, Concat, Coalesce
+from django.db.models import F, Count, Q, Sum, Avg
 from pprint import pprint as pp
 import random
 
 
 def run():
-    r = Restaurant.objects.order_by(F("capacity").asc(nulls_last=True)).values(
-        "name", "capacity"
+
+    print(
+        Rating.objects.filter(rating__lt=0).aggregate(
+            avg_rating=Coalesce(Sum("rating"), 0)
+        )
     )
-    print(r)
+
+    print(Restaurant.objects.aggregate(total_cap=Sum("capacity", default=0)))
     pp(connection.queries)
 
 
